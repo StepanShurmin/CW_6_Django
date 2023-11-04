feae3f7bed36d6ac7e9c13c7179cbbe352399b9d
StepanShurmin <shurmin.sa@gmail.com>
2023-11-04 14:14:17 +0400

    Сделаны модели и контроллеры.

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,97 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView, UpdateView, DeleteView, CreateView
 
-# Create your views here.
+from mailing.forms import MailingMessageForm, MailingSettingsForm, ClientForm
+from mailing.models import MailingSettings, Client, MailingMessage
+
+
+def index(request):
+    return render(request, 'mailing/index.html')
+
+
+class ClientListView(ListView):
+    model = Client
+    template_name = 'mailing/client_list.html'
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class MailingSettingsListView(ListView):
+    model = MailingSettings
+
+
+class MailingSettingsCreateView(CreateView):
+    model = MailingSettings
+    form_class = MailingSettingsForm
+    success_url = reverse_lazy('mailing:mailing_settings_list')
+
+
+class MailingSettingsUpdateView(UpdateView):
+    model = MailingSettings
+    form_class = MailingSettingsForm
+    success_url = reverse_lazy('mailing:mailing_settings_list')
+
+
+class MailingSettingsDeleteView(DeleteView):
+    model = MailingSettings
+    success_url = reverse_lazy('mailing:mailing_settings_list')
+
+
+class MailingMessageListView(ListView):
+    model = MailingMessage
+    template_name = 'mailing/messages.html'
+
+
+class MailingMessageCreateView(CreateView):
+    model = MailingMessage
+    form_class = MailingMessageForm
+    template_name = 'mailing/message_form.html'
+    success_url = reverse_lazy('messages')
+
+
+class MailingMessageUpdateView(UpdateView):
+    model = MailingMessage
+    form_class = MailingMessageForm
+    template_name = 'mailing/message_form.html'
+    success_url = reverse_lazy('messages')
+
+
+class MailingMessageDeleteView(DeleteView):
+    model = MailingMessage
+    template_name = 'mailing/message_confirm_delete.html'
+    success_url = reverse_lazy('messages')
+
+
+class ContactsTemplateView(TemplateView):
+    template_name = 'mailing/contacts.html'
+    extra_context = {
+        'title': 'Контакты'
+    }
+
+    def post(self, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        if self.request.method == 'POST':
+            name = self.request.POST.get('name')
+            email = self.request.POST.get('email')
+            message = self.request.POST.get('message')
+            print(f'New message from {name}, {email}: {message}')
+        return super().get_context_data(**kwargs)
