c031b3e7a7b17d8de2be9796c82c4bb43906869e
StepanShurmin <shurmin.sa@gmail.com>
2023-11-04 16:02:30 +0400

    Добавлена возможность регистрации пользователя.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from mailing.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    avatar = models.ImageField(upload_to='users/avatars', verbose_name='Аватар', **NULLABLE)
+    phone = models.CharField(max_length=55, verbose_name='Телефон', **NULLABLE)
+    country = models.CharField(max_length=100, verbose_name='Страна', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
